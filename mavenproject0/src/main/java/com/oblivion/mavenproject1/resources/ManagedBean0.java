c2d62803872e442c50232b209edf951422f83cef
TeveredziSJ <58471235+MrWorldEnd@users.noreply.github.com>
2022-06-08 03:30:09 +0100

    initial commit
    x

---
+++ mavenproject0/src/main/java/com/oblivion/mavenproject1/resources/ManagedBean0.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.oblivion.mavenproject1.resources;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author st1259
+ */
+@Named(value = "managedBean0")
+@RequestScoped
+public class ManagedBean0 {
+
+    /**
+     * Creates a new instance of ManagedBean0
+     */
+    public ManagedBean0() {
+    }
+    
+}
